ffd50a56aed5e32f4bd124ffa5116aa6a9fde4e6
Aphrodite31135 <hurrry77@naver.com>
2023-03-09 12:47:09 +0900

    2023-03-09

---
+++ 0307/0307.py
@@ -0,0 +1,30 @@
+a=1
+a+=1 # (a+=1) == (a=a+1)
+print(a)
+
+'''
+a=b=c= 10
+print(a,b,c)
+a,b,c= 1,2,3
+print(a,b,c)
+a,b= b,a
+print(a,b,c)
+'''
+
+'''
+a= 10
+print(a)
+b= a
+print(b)
+b= 5
+print(a,b)
+'''
+
+'''
+a= 10
+b= 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+'''
