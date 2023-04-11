b0d0f37c9751a06607c4e6b3ddaa07ae02793703
Aphrodite31135 <hurrry77@naver.com>
2023-04-11 14:45:22 +0900

    2023-04-11

---
+++ 0411/0411.py
@@ -0,0 +1,61 @@
+i = "ajkdlfjlkadjfdklfjk124u34jkdljflkdjfl43223jkldjfkldjkl12j1l0044jkldjklf1812j1k2jlkdjklfjdf8333jk4ljkldjflkd893jkldjfkldjf8922823jk2l3jkljdklfjdlfd9823232j32jdjflkdjf9823"
+l = list(i)
+setting_number=0
+for j in l :
+    if l[j].
+
+# [Quiz (EASY)] 숫자 세기 | Counting digits
+# string = "ajkdlfjlkadjfdklfjk124u34jkdljflkdjfl43223jkldjfkldjkl12j1l0044jkldjklf1812j1k2jlkdjklfjdf8333jk4ljkldjflkd893jkldjfkldjf8922823jk2l3jkljdklfjdlfd9823232j32jdjflkdjf9823"
+# result = 0
+# for i in string :
+#     if i.isnumeric() == True :
+#         result+=int(i)
+# print(result)
+
+# def function1(str) :
+#     # print(str(1111)) # Dangerous Code :(
+#     return str.replace(" ", "")
+# print(function1("다나카"))
+
+# def plus(a,b,/,*,c,d) : # a b Positional Only, c d Keyword Only
+#     return a+b+c+d
+# # print(plus(1,2,3,4)) # Error
+# print(plus(1,2,c=3,d=4)) # or plus(1, 2, d=4, c=3)
+
+# def plus(*,a,b) :
+#     return a+b
+# # print(plus(1,2)) # Error
+# print(plus(a=1,b=2))
+
+# def func(posq, pos2,/,pos_orkwd,*,kwd1,kwd2) # Positional Only, Positional or Keyword, Keyword Only
+
+# print("{:.9f}".format(x/y))
+# def func(a, *b, **c) :
+#     print(a)
+#     print(b)
+#     print(c)
+# func(1,2,3,4,5,"Hello","world!",Hong="gil-doing")
+
+# 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+# a, b = input().split(" ")
+# a=int(a)
+# b=int(b)
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+
+# print("우효 초 럭키다제", sep="*",end="~")
+
+# print(help(print))
+
+# def func(n1, n2, *n3): 
+#     print(n1)
+#     print(n2)
+#     print(n3)
+# func(1,2,3,4,5)
+
+# x, y = input().split(" ")
+# x=int(x)
+# y=int(y)
