e86f44e53ea28ad13a36feff6963962bdd2480d2
Aphrodite31135 <hurrry77@naver.com>
2023-09-13 17:37:09 +0900

    2023-09-13

---
+++ 0913/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+	auto count = 0;
+	auto ch = 'Z';
+	auto limit = 100.0;
+	std::cout << typeid(count).name() << typeid(ch).name() << typeid(limit).name();
+}
+
+//int main() {
+//	enum class Animal {qw, we, er, rt, ty};
+//	std::cout << (int)Animal::qw << (int)Animal::we << (int)Animal::er << (int)Animal::rt << (int)Animal::ty;
+//}
+
+//int main() {
+//	enum Animal {qw, we, er, rt, ty};
+//	std::cout << qw << we << er << rt << ty;
+//}
+
+//int main() {
+//	std::cout << 'A' + 1;
+//	std::cout << int('A' + 1);
+//	std::cout << char('A' + 1);
+//	std::cout << char(66);
+//}
