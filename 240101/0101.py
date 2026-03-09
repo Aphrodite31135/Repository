1dc7320e70f268a8941b4439c20a9cafe93a55dc
Aphrodite31135 <hurrry77@naver.com>
2026-03-10 06:52:29 +0900

    something changed(?)

--- 240101/0101.py
+++ 240101/0101.py
@@ -1,24 +1,75 @@
-test_case=0
-test_list=[]
+# No. 2292
 
-def Cal(n1, n2) :
-    count = n2-n1
-    num=1
-    for i in range(n2,count,-1) :
-        num*=n2
-        n2-=1
-    return num
+# N=int(input())
+# count=1
+# cur=1
+# while(cur<N):
+#     if(cur==N):
+#         break
+#     else:
+#         cur+=6*count
+#         count+=1
+# print(count)
 
-def Fac(n) :
-    num = 1
-    for i in range(1,n+1) :
-        num*=i
-    return num
+# No. 2231
 
-test_case=int(input())
-for i in range(test_case) :
-    nm1, nm2 = map(int, input().split())
-    test_list.append([nm1, nm2])
+# N=int(input())
+# result=0
+# for i in range(1,N):
+#     temp=0
+#     string=str(i)
+#     for j in string:
+#         temp+=int(j)
+#     if(i+temp==N):
+#         result=i
+#         break
+# print(result)
 
-for lst in test_list :
-    print(int(Cal(lst[0], lst[1])/Fac(lst[0])))
+
+# No. 1978
+
+# N=int(input())
+# nums=list(map(int,input().split()))
+# count=0
+# for i in nums:
+#     if i==2:
+#         count+=1
+#     for j in range(2,i):
+#         if(i%j==0):
+#             break
+#         if(j==i-1):
+#             count+=1
+# print(count)
+
+
+# No.30802
+
+# N=int(input())
+# lst=list(map(int,input().split()))
+# T,P = map(int, input().split())
+# result=0
+# for i in lst:
+#     if i%T==0:
+#         result+=int(i/T)
+#     else:
+#         result+=int(i/T+1)
+# print(result)
+# print(f"{int(N/P)} {int(N%P)}")
+
+
+# No. 4153
+
+# lst = list()
+# while(True):
+#     lst.append(list(map(int, input().split())))
+#     if lst[-1]==[0,0,0]:
+#         lst.pop()
+#         break
+# import math
+# answer = list()
+# for i in lst:
+#     i.sort()
+#     if (pow(i[0],2)+pow(i[1],2))==pow(i[2],2):
+#         print("right")
+#     else:
+#         print("wrong")
